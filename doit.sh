4b596e15c089e5da8b577a1be06f0851bf4d6c4b
Andy Loftus <aloftus@gmail.com>
2019-09-06 15:59:27 -0500

    Fix regexp for cleanup

--- doit.sh
+++ doit.sh
@@ -133,9 +133,9 @@ do_cleanup() {
     tmpfn_images=$(mktemp)
     tmpfn_containers=$(mktemp)
     docker ps -a -f status=exited -f status=dead --format "{{.ID}} {{.Image}}" \
-    | >/dev/null tee \
-      >( awk '/docker_puppet/{print $1}' > $tmpfn_containers ) \
-      >( awk '/docker_puppet/{print $2}' > $tmpfn_images )
+    | tee >( awk '/docker_(puppet|agent)/{print $1}' > $tmpfn_containers ) \
+          >( awk '/docker_(puppet|agent)/{print $2}' > $tmpfn_images ) \
+          >/dev/null
 
     # rm containers
     xargs -a $tmpfn_containers -r docker rm -f
