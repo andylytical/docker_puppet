e384cf0611b08861a502ba274beb6ebedd547336
Andy Loftus <aloftus@gmail.com>
2020-04-08 11:32:56 -0500

    Add bin/hup
    Call custom bin/* scripts during enc/setup.sh

--- server/enc/setup.sh
+++ server/enc/setup.sh
@@ -8,17 +8,21 @@ cd "${PUPPERWARE:-$DEFAULT}" || {
     exit 1
 }
 
-# install enc
-docker cp server/enc/install.sh pupperware_puppet_1:/install_enc.sh
-docker-compose exec puppet bash -c "/install_enc.sh |tee install_enc.log"
-
-# restart puppetserver to re-read config
-docker-compose exec puppet pkill -HUP -u puppet java
-
-# initialize enc database
-docker cp server/enc/tables.yaml pupperware_puppet_1:/etc/puppetlabs/local/enc/
-docker-compose exec puppet enc_adm --init
-
 # make enc_adm runner script
 /usr/bin/cp -f bin/puppetserver bin/enc_adm
 sed -i -e 's/puppetserver/enc_adm/' bin/enc_adm
+
+# make puppetserver reload script
+/usr/bin/cp -f bin/puppetserver bin/hup
+sed -i -e '/puppetserver/ d' bin/enc_adm
+>>bin/hup echo "docker-compose exec puppet pkill -HUP -u puppet java"
+ln -sf hup bin/reload
+
+# install enc
+docker cp server/enc/install.sh pupperware_puppet_1:/install_enc.sh
+docker-compose exec puppet bash -c "/install_enc.sh |tee install_enc.log"
+bin/hup
+
+# initialize enc database
+docker cp server/enc/tables.yaml pupperware_puppet_1:/etc/puppetlabs/local/enc/
+bin/enc_adm --init
