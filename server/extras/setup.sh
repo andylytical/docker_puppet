88bec3245b3bc71938e48f93cb694f97ef953629
Andy Loftus <aloftus@gmail.com>
2020-11-12 17:09:35 -0600

    restart puppetserver after installing toml-rb

--- server/extras/setup.sh
+++ server/extras/setup.sh
@@ -6,6 +6,13 @@ PW="${PUPPERWARE:-$HOME/pupperware}"
 
 install_toml_rb() {
     "$PW/bin/puppetserver" gem install toml-rb
+    "$PW/bin/hup"
+}
+
+restart_puppetserver() {
+    "$PW/bin/hup"
 }
 
 install_toml_rb
+
+restart_puppetserver
